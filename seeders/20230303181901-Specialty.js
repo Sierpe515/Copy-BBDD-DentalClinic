aea267b76cafd82a841b3107534204c94f1cd024
Sierpe <feroa515@hotmail.com>
2023-03-15 23:18:14 +0100

    feat: employee seeder

--- seeders/20230303181901-Specialty.js
+++ seeders/20230303181901-Specialty.js
@@ -5,11 +5,11 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Specialties',
     [
-    {id: 1,type: "implantology",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
-    {id: 2,type: "denture",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
-    {id: 3,type: "orthodontics",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
-    {id: 4,type: "general orthodontics",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
-    {id: 5,type: "dental aesthetics",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"}
+    {id: 1,type: "General Dentist",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+    {id: 2,type: "Orthodontist",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+    {id: 3,type: "Periodontist",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+    {id: 4,type: "Dental Assistant",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+    {id: 5,type: "Administrative",createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"}
  ],
   {});
  },
---
+++ seeders/20230315152914-Employee.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Employees',
+    [
+      {id: 1, user_id: 5, specialty_id: 1, image: "", collegiate_number: "num0001", schedule: "Mañana", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+      {id: 2, user_id: 6, specialty_id: 1, image: "", collegiate_number: "num0002", schedule: "Mañana", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+      {id: 3, user_id: 7, specialty_id: 1, image: "", collegiate_number: "num0003", schedule: "Tarde", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+      {id: 4, user_id: 8, specialty_id: 1, image: "", collegiate_number: "num0004", schedule: "Tarde", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+      {id: 5, user_id: 9, specialty_id: 1, image: "", collegiate_number: "num0005", schedule: "Tarde", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+ ],
+  {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
